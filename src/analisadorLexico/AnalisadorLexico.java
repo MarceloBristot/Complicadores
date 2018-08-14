77ba712dfd8b31f554e39c220408bdf35ff973c2
MarceloBristot <31294536+MarceloBristot@users.noreply.github.com>
2018-08-14 00:35:49 -0300

    Add files via upload

---
+++ src/analisadorLexico/AnalisadorLexico.java
@@ -0,0 +1,13 @@
+package analisadorLexico;
+
+import javax.swing.*;
+
+public class AnalisadorLexico extends JFrame {
+    public AnalisadorLexico(){
+        setTitle("Compilador");
+        setResizable(false);
+        setBounds(500,150,500,500);
+        setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        System.out.println("Olá mundo!");
+    }
+}
---
+++ src/app/App.java
@@ -0,0 +1,9 @@
+package app;
+
+import analisadorLexico.AnalisadorLexico;
+
+public class App {
+    public static void main(String[] args) {
+        new AnalisadorLexico().setVisible(true);
+    }
+}
