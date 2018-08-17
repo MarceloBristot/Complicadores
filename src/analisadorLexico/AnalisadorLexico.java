a02af7eb81541019f00bab58a1f5f8ccb168bba5
marcelo <marcelombfilho@outlook.com>
2018-08-16 21:30:01 -0300

    Alterações na tela.

--- src/analisadorLexico/AnalisadorLexico.java
+++ src/analisadorLexico/AnalisadorLexico.java
@@ -4,10 +4,11 @@ import javax.swing.*;
 
 public class AnalisadorLexico extends JFrame {
     public AnalisadorLexico(){
+
+        //Configurações da tela
         setTitle("Compilador");
         setResizable(false);
-        setBounds(500,150,500,500);
+        setBounds(150,50,900,600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        System.out.println("Olá mundo!");
     }
 }
