0de8c19298bef033dc33c4ba7195959f7c65d709
marcelo <marcelombfilho@outlook.com>
2018-08-17 00:24:45 -0300

    Alterações na tela, novas funções.

--- src/analisadorLexico/AnalisadorLexico.java
+++ src/analisadorLexico/AnalisadorLexico.java
@@ -1,14 +1,39 @@
 package analisadorLexico;
 
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import java.awt.*;
 
 public class AnalisadorLexico extends JFrame {
-    public AnalisadorLexico(){
+
+    private JTextArea text = new JTextArea("Teste...");
+    private JMenuBar menuBar = new JMenuBar();
+    private JMenu menu = new JMenu("Arquivo");
+
+    public AnalisadorLexico() {
 
         //Configurações da tela
-        setTitle("Compilador");
+        super("Compilador");
         setResizable(false);
-        setBounds(150,50,900,600);
+        setBounds(150, 50, 900, 600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        //Container para incluir recursos
+        Container c = this.getContentPane();
+        c.setLayout(null);
+        setContentPane(c);
+
+        //Barra de opções
+        menuBar.add(menu);
+        setJMenuBar(menuBar);
+
+
+
+        //Caixa de texto para edição
+        text.setBounds(0,0,900,600);
+        c.add(text);
+
+
+
     }
 }
